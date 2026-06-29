15ea8143408c576018195776c6e7b428608b6c37
RadiantLumina <Rong-0202@outlook.com>
2026-06-29 18:08:41 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,6 @@ A professional LLM evaluation framework
 
 __version__ = "0.1.0"
 __author__ = "RadiantLumina"
-__email__ = "Rong-0202@outlook.com"
 
 from .metrics.base_metric import BaseMetric
 from .evaluators.base_evaluator import BaseEvaluator
